74ab7e122054f2817b881115b4de18b84de36e10
MrRedPortal <MrRedPortal@users.noreply.github.com>
2022-04-02 17:06:06 +1100

    Mostly working

--- cell.js
+++ cell.js
@@ -1,6 +1,85 @@
-function cell(){
+function Cell(i,j,w){
+    this.i = i;
+    this.j = j;
+    this.x = i*w;
+    this.y = j*w;
+    this.w = w;
+    this.neighborCount=0;
+    this.mine = false;
 
-    this.mine = true;
+    this.revealed = false;
+
+}
+
+Cell.prototype.show = function(){
+    stroke(0);
+    noFill();
+    rect(this.x, this.y, this.w, this.w)
+
+    if(this.revealed){
+        if(this.mine){
+            stroke(0);
+            fill(100);
+            ellipse(this.x+w*0.5, this.y+w*0.5, this.w*0.5);
+        } else {
+            fill(200);
+            stroke(0);
+            rect(this.x,this.y,this.w);
+            if(this.neighborCount>0) {
+                textAlign(CENTER);
+                fill(0);
+                text(this.neighborCount, this.x + this.w * 0.5, this.y + this.w - 6);
+            }
+        }
+    }
+}
+
+Cell.prototype.countNeighbors = function(){
+    if(this.mine){
+        this.neighborCount=-1;
+        return;
+    }
+    var total=0;
+    for (var xOff=-1; xOff<=1;xOff++){
+        for(var yOff=-1; yOff<=1;yOff++){
+            var i = this.i+xOff;
+            var j = this.j+yOff;
+            if(i>-1&&i<col&&j>-1&&j<row) {
+                var neighbor = grid[i][j];
+                if (neighbor.mine) {
+                    total++;
+                }
+            }
+        }
+    }
+    console.log(total);
+    this.neighborCount=total;
+}
+
+Cell.prototype.contains = function (mouseX,mouseY) {
+    return (mouseX>this.x && mouseX<this.x+this.w &&
+        mouseY>this.y && mouseY<this.y+this.w);
+}
+
+Cell.prototype.reveal = function(){
     this.revealed = true;
+    if(this.neighborCount == 0){
+        this.floodFill();
+    }
+}
 
+
+Cell.prototype.floodFill = function () {
+    for (var xOff=-1; xOff<=1;xOff++){
+        for(var yOff=-1; yOff<=1;yOff++){
+            var i = this.i+xOff;
+            var j = this.j+yOff;
+            if(i>-1&&i<col&&j>-1&&j<row) {
+                var neighbor = grid[i][j];
+                if(!neighbor.mine && !neighbor.revealed){
+                    neighbor.reveal();
+                }
+            }
+        }
+    }
 }
--- sketch.js
+++ sketch.js
@@ -1,24 +1,64 @@
 
 //globals
 var grid;
-var col = 20;
-var row = 20;
+var col;
+var row;
+var w = 20;
+var totalMines = 40;
 //global end
 
 function setup() {
-    createCanvas(400,400);
+    createCanvas(401,401);
+    col = floor(width/w);
+    row = floor(height/w);
     grid = make2DArray(col,row);
+    //Minefield Creation Loop
     for (var i=0; i<col; i++){
         for (var j=0; j<row;j++){
-            grid[i][j] = new cell();
+            grid[i][j] = new Cell(i,j, w);
         }
     }
 
+    //Mine placement loop
+    var placedMines = 0;
+    while( placedMines !== totalMines){
+        var i = floor(random(col));
+        var j = floor(random(row));
+        if(!grid[i][j].mine){
+            grid[i][j].mine = true;
+            placedMines++
+        }
+    }
+
+    //Mine Neighbour Count loop
+    for (var i=0; i<col; i++){
+        for (var j=0; j<row;j++){
+            grid[i][j].countNeighbors();
+        }
+    }
 }
+
+
+function mousePressed() {
+    for (var i=0; i<col; i++){
+        for (var j=0; j<row;j++){
+            if (grid[i][j].contains(mouseX,mouseY)){
+             grid[i][j].reveal();
+
+             if(grid[i][j].mine){
+                 gameOver();
+             }
+
+            }
+        }
+    }
+}
+    
+
 
 function draw() {
 
-  background(0);
+  background(255);
     for (var i=0; i<col; i++){
         for (var j=0; j<row;j++){
             grid[i][j].show();
@@ -33,5 +73,14 @@ function make2DArray(col,row) {
     for(var i = 0; i < array.length; i++){
         array[i] = new Array(row);
     }
+    return array;
 
+}
+
+function gameOver(){
+    for (var i=0; i<col; i++) {
+        for (var j = 0; j < row; j++) {
+            grid[i][j].revealed=true;
+        }
+    }
 }
